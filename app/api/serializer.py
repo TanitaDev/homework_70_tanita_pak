9e591afd723ea925ee980cc284e8dcdf75f7c8ad
tanitaDev <tanita.pak@gmail.com>
2022-11-21 03:24:59 +0600

    написала сериализатор для модели Task

--- app/api/serializer.py
+++ app/api/serializer.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers
 
+from webapp.models import Task
+
 
 class TaskSerializer(serializers.Serializer):
-    pass
+    class Meta:
+        model = Task
+        fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at', 'project']
+        read_only_fields = ['created_at']
